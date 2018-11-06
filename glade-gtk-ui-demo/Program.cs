9fa15d8ab7e961d20f0e3447e97c22e294827038
Niel Archer <niel.archer@gmail.com>
2018-11-06 23:16:23 +0000

    Initial commit.

---
+++ glade-gtk-ui-demo/Program.cs
@@ -0,0 +1,33 @@
+﻿using Gtk;
+using System;
+
+namespace glade_gtk_ui_demo
+{
+	public class ShowWindow
+	{
+		public static void Main(string[] args)
+		{
+			new ShowWindow();
+		}
+
+		static void OnAppWindowDestroy(object o, EventArgs args)
+		{
+			
+			Gtk.Application.Quit();
+		}
+
+		static void OnQuitButtonEvent(object o, ButtonReleaseEventArgs args)
+		{
+			Gtk.Application.Quit();
+		}
+
+		private ShowWindow()
+		{
+			Gtk.Application.Init();
+			Builder gui = new Builder();
+			gui.AddFromFile("/home/niel/dev/c-sharp/glade-gtk-ui-demo/glade-gtk-ui-demo/main.glade");
+			gui.Autoconnect(this);
+			Gtk.Application.Run();
+		}
+	}
+}
